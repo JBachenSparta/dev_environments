f0f38e411daacf3cfd493c1181fbf6225f714a44
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2019-05-13 17:04:15 +0100

    first commit

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+sudo npm install -g pm2
+
+cd /app
+pm2 start app.js
+vagrant ssh
