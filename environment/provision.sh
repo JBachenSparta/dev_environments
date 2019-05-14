fe76e395fd15497874e0796d4356c862b7aff149
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2019-05-14 11:53:00 +0100

    updated provision.sh

--- environment/provision.sh
+++ environment/provision.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo apt-get install nginx -y
-
+sudo apt-get upgrade -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs -y
-
-sudo npm install -g pm2
-
-cd /app
+sudo apt-get install -y nodejs
+npm install -g pm2
+cd /app/
+npm install
 pm2 start app.js
